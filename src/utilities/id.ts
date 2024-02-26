523e7321bf7cb39c36c7cb76b2b5757d261342d7
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2024-02-26 23:14:58 +0100

    build: replace `nanoid` with `cuid2`

--- src/utilities/id.ts
+++ src/utilities/id.ts
@@ -1,15 +1,7 @@
-import { customAlphabet } from "nanoid";
+import { init } from "@paralleldrive/cuid2";
 
 import { environment } from "@/environment.mjs";
 
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const alphanumericLowerCase = `01234567890${lowerCase}`;
-
-const generateLowerCase = customAlphabet(lowerCase, 1);
-const generateAlphanumericLowerCase = customAlphabet(
-  alphanumericLowerCase,
-  environment.NEXT_PUBLIC_ID_LENGTH - 1,
-);
-
-export const generateId = () =>
-  `${generateLowerCase()}${generateAlphanumericLowerCase()}`;
+export const generateId = init({
+  length: environment.NEXT_PUBLIC_ID_LENGTH,
+});
